02f08d3bb529fbd9223f9d04f2dded5dc90b19e6
Thaís Oliveira <jts_oliveira@outlook.com>
2022-08-05 19:24:36 -0300

    config style dictonary to generate css variables

---
+++ design-tokens/src/brand.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const fs = require("fs");
+
+function getDirectories(dirPath) {
+  return fs.readdirSync(path.resolve(__dirname, dirPath)).map((folder) => {
+    return folder;
+  });
+}
+
+function getBrands() {
+  const BRANDS = [
+    {
+      source: path.resolve("tokens", "globals", "**", "*.json"),
+      dest: "",
+      filename: "globals",
+      brand: "",
+      theme: "",
+      mode: "",
+    },
+  ];
+
+  getDirectories(path.resolve("tokens", "brands")).map((brand) => {
+    getDirectories(path.resolve("tokens", "brands", brand)).map((theme) => {
+      getDirectories(path.resolve("tokens", "brands", brand, theme)).map(
+        (mode) => {
+          BRANDS.push({
+            source: path.resolve(
+              "tokens",
+              "brands",
+              brand,
+              theme,
+              mode,
+              "**",
+              "*.json"
+            ),
+            dest: path.join(brand, theme),
+            filename: mode,
+            brand: brand,
+            theme: theme,
+            mode: mode,
+          });
+        }
+      );
+    });
+  });
+
+  return BRANDS;
+}
+
+module.exports = {
+  getBrands,
+};
---
+++ design-tokens/src/build.js
@@ -0,0 +1,14 @@
+const StyleDictionaryPackage = require("style-dictionary");
+const { registerConfig } = require("./config");
+
+async function buildTokens({ current, buildPath }) {
+  const StyleDictionary = StyleDictionaryPackage.extend(
+    registerConfig({ current: current, buildPath: buildPath })
+  );
+
+  StyleDictionary.buildAllPlatforms();
+}
+
+module.exports = {
+  buildTokens,
+};
