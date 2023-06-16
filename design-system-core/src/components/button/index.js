882e74e3eeee1051291637e4be26a824f9120f21
Thaís Oliveira <jts_oliveira@outlook.com>
2023-06-16 14:45:46 -0300

    styling button

--- design-system-core/src/components/button/index.js
+++ design-system-core/src/components/button/index.js
@@ -1,17 +1,50 @@
 import { LitElement, html, unsafeCSS } from "lit";
 import style from "./style.scss";
+import { classMap } from "lit/directives/class-map.js";
+import { ifDefined } from "lit/directives/if-defined.js";
 
 export class DscButton extends LitElement {
   static get styles() {
     return unsafeCSS(style);
   }
 
+  static get properties() {
+    return {
+      disabled: { type: Boolean },
+      loading: { type: Boolean },
+    };
+  }
+
   constructor() {
     super();
+    this.disabled = false;
+    this.loading = false;
+  }
+
+  _handleClick() {
+    this.dispatchEvent(
+      new CustomEvent("dscClick", {
+        bubbles: true,
+        composed: true,
+      })
+    );
   }
 
   render() {
-    return html`<button><slot></slot></button>`;
+    return html`
+      <button
+        class=${classMap({
+          [`button`]: true,
+          [`button-loading`]: this.loading,
+        })}
+        aria-disabled="${this.disabled}"
+        aria-label="${ifDefined(this.loading ? "Loading" : undefined)}"
+        ?disabled=${this.disabled}
+        @click=${this._handleClick}
+      >
+        <slot></slot>
+      </button>
+    `;
   }
 }
 
